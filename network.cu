af54aec0054cca8d9901743edfe13bcc1ec2cb3e
Jan Němec <jnem6403@seznam.cz>
2014-04-14 06:52:13 +0200

    types

--- network.cu
+++ network.cu
@@ -1,5 +1,56 @@
 #include <stdio.h>
 
+/*
+	Global settings
+*/
+
+int HIDDEN_LAYERS = 5;
+int NEURONS_IN_LAYER = 100;
+int EMULATION = 1;
+
+/*
+	Global types
+*/
+
+typedef FLOAT_TYPE float;
+
+/*
+	Data inside of each group (without connections to other groups)
+*/
+typedef struct
+{
+	/* full matrix NEURONS_IN_LAYER * NEURONS_IN_LAYER
+	   weight from 1 to 2 is in w[1 + 2 * NEURONS_IN_LAYER] */
+	FLOAT_TYPE *w;
+	/* 0 .. NEURONS_IN_LAYER */
+	FLOAT_TYPE *tresholds;
+	/* 0 .. NEURONS_IN_LAYER */
+	FLOAT_TYPE *potentials;
+} TGroupInternal;
+
+/* Connection between groups*/
+typedef struct
+{
+	/* from group index */
+	int group;
+	/* from neuron (inside of the group) index */
+	int neuron;
+	/* weight */
+	FLOAT_TYPE w;
+} TConnection;
+
+/* Group inluding without connections to other groups */
+typedef struct
+{
+	/* Internal group data */
+	TGroupInternal inside;
+	/* Connection inside the group
+	   connections[1][2] is the third (0, 1, 2) connection of the second (0, 1)
+       neuron */
+	TConnection **connections;
+}
+
+
 // Kernel definition
 __global__ void VecAdd(float* A, float* B, float* C)
 {
@@ -7,7 +58,7 @@ __global__ void VecAdd(float* A, float* B, float* C)
 	C[i] = A[i] + B[i];
 }
 
-#define N 10
+
 
 int main(void)
 {
