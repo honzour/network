5c086a3de64817d82c03022b6f66dd108ec12953
Jan Němec <jnem6403@seznam.cz>
2014-04-14 06:27:45 +0200

    cosmetis

--- network.cu
+++ network.cu
@@ -3,28 +3,28 @@
 // Kernel definition
 __global__ void VecAdd(float* A, float* B, float* C)
 {
-int i = threadIdx.x;
-C[i] = A[i] + B[i];
+	int i = threadIdx.x;
+	C[i] = A[i] + B[i];
 }
 
 #define N 10
 
 int main(void)
 {
-float A[N];
-float B[N];
-float C[N];
+	float A[N];
+	float B[N];
+	float C[N];
 
-int i;
+	int i;
 
-for (i = 0; i < N; i++)
-{
-A[i]=i; B[i]=10;
-}
+	for (i = 0; i < N; i++)
+	{
+		A[i]=i; B[i]=10;
+	}
 
-VecAdd<<<1, N>>>(A, B, C);
+	VecAdd<<<1, N>>>(A, B, C);
 
-printf("je to %i\n", (int)A[5]);
+	printf("je to %i\n", (int)A[5]);
 
-return 0;
+	return 0;
 }
